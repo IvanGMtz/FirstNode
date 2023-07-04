51a0ca916df1c288d616848f0cf694fcf75deac6
IvanGMtz <navi.garces24@gmail.com>
2023-07-04 16:44:38 -0500

    feat: :sparkles: Import module from node

--- app.js
+++ app.js
@@ -1,44 +1,15 @@
-// escribe en la consola
-console.log("hello world");
-// muestra un objeto vacio ya que node es orientado de esta forma 
-console.log(this);
-// muestra la configuracion de node y es manipulable
-console.log(global);
-// variables de entorno configuracion ruta etc
-console.log(process);
-// ES UN TIPO DE COMANDO QUE PERMITEN MODIFICAR LAS CREDENCIALES DEL SISTEMA CMO UN OBJETO
-console.log(process.env.PROCESSOR_LEVEL)
-// global es una configuracion que le dice que muestre que tiene instalado el node para trabajar con el js
-console.log(global)
+// npm init -y  para crear package.json
+// Se debe agregar al package.json type module
+import readline from 'node:readline';
 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-// para solicitar datos por medio de la consola con el metodo stdin
-// process.stdin.setEncording('utf8');// es para decirle a la consola que sean en utf8 
-process.stdin.on("data", (input)=>{
-    console.clear();// limpia la consola
-    console.log(`data resivida ${input}`);
-    process.exit();// rompe el proceso de forma singular
-})
-// otra forma de hacerlo
-let input = process.stdin;// esta dejando el process 
-input.setEncording('utf8');
-input.on("data", (chuck)=>{
-    console.clear();
-    console.log(`data resivida ${chuck}`);
-    process.exit();
-})
-
-
-// obtener los datos en linea usando las promesas de node
-const readline = require('node:readline/promises');// require: obtiene los datos importandolos del modulo
-
-
-//requerir modulo en node exportado por default
-const config = require('./modulo')
-
-
-//requerir un dato json
-const json = require('./data.json')
-
-
-// requerimiento de node para traer el modulo nativo de node basado en bash y C 
+rl.question('Ingrese el nombre?\n', (nombre)=>{
+    readline.cursorTo(process.stdout, 0, 1); //Posicion del cursor
+    readline.clearScreenDown(process.stdout); // Eliminar la linea de comando
+    console.log(`Nombre: ${nombre}`);
+    rl.close(); //Salir del todo el proceso
+});
